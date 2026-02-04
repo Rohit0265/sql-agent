180aa3e85500312415dc036fcca6d4eb6a14c5f5
Rohit Mathur <116487107+Rohit0265@users.noreply.github.com>
2026-02-04 22:33:37 +0530

    new add

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,40 @@
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import { z } from 'zod/v4';
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query their database using natural language.
+
+You have access to following tools:
+1. schema tool - call this tool to get the database schema which will help you to write sql query.
+2. db tool - call this tool to query the database.
+
+Rules:
+- Generate ONLY SELECT queries (no INSERT, UPDATE, DELETE, DROP)
+- Always use the schema provided by the schema tool
+- Return valid SQLite syntax
+
+Always respond in a helpful, conversational tone while being technically accurate.`;
+
+
+  const result = streamText({
+    model: "openai/gpt-5.2-codex",
+    messages: await convertToModelMessages(messages),
+    system:SYSTEM_PROMPT,
+        tools: {
+      db: tool({
+        description: 'Call this tool to query a database',
+        inputSchema: z.object({
+          query: z.string().describe('The SQL query to be ran'),
+        }),
+        execute: async ({query})=>{
+            console.log('Query',query);
+            return '';
+        }
+      }),
+    },
+  });
+
+  return result.toUIMessageStreamResponse();
+}
